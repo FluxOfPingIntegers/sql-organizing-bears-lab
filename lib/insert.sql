745afbc3d54fdcb44abefd4a5a7a1504d8ef5fd0
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-04-01 09:44:17 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+    ('Mr. Chocolate', 10, 'Male', 'Brown', 'Sweet', 1),
+    ('Rowdy', 5, 'Male', 'Red', 'Wild', 1),
+    ('Tabitha', 4, 'Female', 'Brown', 'Curious', 1),
+    ('Sergeant Brown', 8, 'Male', 'Black', 'Aggressive', 0),
+    ('Melissa', 3, 'Female', 'Black', 'Happy', 1),
+    ('Grinch', 5, 'Neutral', 'Red', 'Hungry', 1),
+    ('Wendy', 15, 'Female', 'White', 'Tired', 1),
+    (null, 12, 'Gender-Queer', 'Brown', 'Cautious', 1);
